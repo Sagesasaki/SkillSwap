e34c24b100e492d7ad2f7a6811600164bd0d70db
abi-murthy <105751256+abi-murthy@users.noreply.github.com>
2023-12-01 02:41:49 -0800

    added chatroom servlet

---
+++ javaproject/src/main/java/servlets/GetChatRoomMessagesServlet.java
@@ -0,0 +1,50 @@
+package servlets;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+import java.io.Console;
+import java.io.IOException;
+import java.util.List;
+import com.google.gson.*;
+import model.ChatRoomMessage;
+import dao.ChatRoomMessageDAO;
+
+import java.lang.reflect.Type;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@WebServlet("/getChatRoomMessages")
+public class GetChatRoomMessagesServlet extends HttpServlet {
+	private ChatRoomMessageDAO messagesDao = new ChatRoomMessageDAO();
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		List<ChatRoomMessage> messages = messagesDao.loadAllMessages();
+		Gson gson = new GsonBuilder()
+		        .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
+		        .create();
+		String messagesJson = gson.toJson(messages);
+
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		response.getWriter().write(messagesJson);
+		System.out.println(messagesJson);
+	}
+
+	public class LocalDateTimeAdapter implements JsonSerializer<LocalDateTime>, JsonDeserializer<LocalDateTime> {
+		private static final DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+
+		@Override
+		public JsonElement serialize(LocalDateTime localDateTime, Type srcType, JsonSerializationContext context) {
+			return new JsonPrimitive(formatter.format(localDateTime));
+		}
+
+		@Override
+		public LocalDateTime deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+				throws JsonParseException {
+			return LocalDateTime.parse(json.getAsString(), formatter);
+		}
+	}
+}
